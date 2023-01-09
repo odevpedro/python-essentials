0907f21debf4f5812ecce66fc346b3e2a308bbe0
odevepdro <pedrosschmidt2@gmail.com>
2023-01-09 18:45:57 -0300

    data types

--- hello.py
+++ hello.py
@@ -1,41 +1,26 @@
-# This is a comment. Anything on a line after a "#" symbol is ignored by the interpreter.
+# Numeric types
+my_int = 42  # integer
+my_float = 3.14  # float
+my_complex = 3 + 4j  # complex
 
-# Variables can be declared and assigned values using the "=" operator.
-# The type of the variable is determined by the value it is assigned.
-my_string = "Hello, world!"  # This is a string.
-my_int = 42  # This is an integer.
-my_float = 3.14  # This is a float (a decimal number).
-my_bool = True  # This is a boolean value (True or False).
+# Sequence types
+my_list = [1, 2, 3]  # list
+my_tuple = (1, 2, 3)  # tuple
+my_range = range(3)  # range
 
-# We can perform basic arithmetic operations on numbers.
-result = 3 + 4  # Addition
-result = 4 - 3  # Subtraction
-result = 3 * 4  # Multiplication
-result = 4 / 3  # Division
-result = 4 % 3  # Modulus (remainder of division)
+# Text type
+my_string = "Hello, world!"  # string
 
-# We can use the "print()" function to output text to the console.
-print(my_string)
-print(my_int)
-print(my_float)
-print(my_bool)
+# Mapping type
+my_dict = {'a': 1, 'b': 2, 'c': 3}  # dictionary
 
-# We can use the "input()" function to get input from the user.
-name = input("What is your name? ")
-print(f"Hello, {name}!")  # The "f" in front of the string indicates that it is a formatted string.
+# Set types
+my_set = {1, 2, 3}  # set
+my_frozenset = frozenset({1, 2, 3})  # frozenset
 
-# We can use "if" statements to control the flow of our code based on a condition.
-if my_int > 10:
-  print("The integer is greater than 10.")
-else:
-  print("The integer is not greater than 10.")
+# Boolean type
+my_bool = True  # boolean
 
-# We can use "for" loops to repeat a block of code for a given number of iterations.
-for i in range(5):  # The "range()" function generates a sequence of numbers from 0 to 4.
-  print(i)
-
-# We can use "while" loops to repeat a block of code until a certain condition is met.
-count = 0
-while count < 5:
-  print(count)
-  count += 1  # The "count += 1" syntax is shorthand for "count = count + 1".
+# Binary types
+my_bytes = b'Hello, world!'  # bytes
+my_bytearray = bytearray(b'Hello, world!')  # bytearray
